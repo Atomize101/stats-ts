d92e1bb4f6a9ed0cc70953e77c3a586d5d36f8f5
Chris Patrick <atomize101@gmail.com>
2021-02-05 09:24:31 -0500

    Composition based approach

--- build/Summary.js
+++ build/Summary.js
@@ -1,1 +1,11 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Summary = void 0;
+var Summary = /** @class */ (function () {
+    function Summary(analyzer, outPutTarget) {
+        this.analyzer = analyzer;
+        this.outPutTarget = outPutTarget;
+    }
+    return Summary;
+}());
+exports.Summary = Summary;
---
+++ src/Summary.ts
@@ -0,0 +1,13 @@
+import { MatchData } from './MatchData';
+
+export interface Analyzer {
+	run(matches: MatchData[]): string;
+}
+
+export interface OutPutTarget {
+	print(report: string): void;
+}
+
+export class Summary {
+	constructor(public analyzer: Analyzer, public outPutTarget: OutPutTarget) {}
+}
